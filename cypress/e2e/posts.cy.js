74aa5475e9b26b5a419547e838318fd3cd2b6790
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-27 09:27:36 -0500

    Escenario E0007 creado exitoso.

--- cypress/e2e/posts.cy.js
+++ cypress/e2e/posts.cy.js
@@ -16,7 +16,7 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   }
 });
 
-describe("Escenarios E0001 - E0003", function () {
+describe("Escenarios E0006 - E0008", function () {
   let randomRow;
 
   beforeEach(() => {
@@ -30,6 +30,90 @@ describe("Escenarios E0001 - E0003", function () {
       cy.screenshot("ss");
       cy.wait(1000);
     });
+  });
+
+  it("E0007 - Unpublish de post previamente creado y se vuelve a publicar", function () {
+    //Given que voy a la sección de posts
+    PrincipalPage.clickPosts();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And el administrador ve la página de listado de posts
+    PostPage.getTitleSection().should("include.text", "Posts");
+
+    //And le da click en el boton New Post
+    PostPage.clickNewPost();
+    cy.screenshot("ss");
+
+    //And el administrador ve la página de creación de post
+    PostPage.creationPostPage().should("have.value", "");
+
+    //And escribe el titulo del post
+    let titulo = dataPool[1].tituloPost;
+    PostPage.writeTitle(titulo);
+    cy.screenshot("ss");
+
+    //And da click en contenido
+    PostPage.clickInContent();
+    cy.wait(1000);
+
+    //And le da click en el boton de Publish
+    PostPage.publishPostButton();
+    cy.wait(1000);
+
+    //And le da click en el boton Continue, final review
+    PostPage.continueButton();
+
+    //And le da click en el boton Publish post, right now
+    PostPage.publishPostButtonFinal();
+
+    //And cierre el modal de confirmación de publicación
+    PostPage.closePublishModal();
+
+    //And debería ver el post publicado en la lista de posts
+    PostPage.lastPostCreated(titulo, "click");
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And doy clic en Unpublish
+    PostPage.unpublishPost();
+    cy.wait(1000);
+    
+    //And doy clic en Unpublish
+    PostPage.unpublishPostFinal();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And le de click en el boton de devolverse a la lista de posts
+    PostPage.clickBackToPosts();
+    cy.wait(1000);
+
+    //And debo ver el post como Draft
+    PostPage.postAsDraft();
+    cy.screenshot("ss");
+
+    //And debería ver el post publicado en la lista de posts
+    PostPage.lastPostCreated(titulo, "click");
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And le da click en el boton de Publish
+    PostPage.publishPostButton();
+    cy.wait(1000);
+
+    //And le da click en el boton Continue, final review
+    PostPage.continueButton();
+
+    //And le da click en el boton Publish post, right now
+    PostPage.publishPostButtonFinal();
+
+    //When cierre el modal de confirmación de publicación
+    PostPage.closePublishModal();
+
+    //Then debería ver el post publicado en la lista de posts
+    PostPage.lastPostCreated(titulo, "notClick");
+    cy.wait(1000);
+    cy.screenshot("ss");
   });
 
   it("E0008 - Unpublish de post previamente creado", function () {
